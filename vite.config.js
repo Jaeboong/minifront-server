8df08f9eab9161a4187269492f568dd8e9a10bdf
unknown <cbkjh0225@gmail.com>
2025-06-02 18:59:43 +0900

    chore: setup production environment and GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', // ← 또는 아예 제거
+  base: '/minifront-server/',
   plugins: [react()],
 });
