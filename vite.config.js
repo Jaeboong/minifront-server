e93f42039af2c5c225b11ece4cc0a68b3e3bddef
unknown <cbkjh0225@gmail.com>
2025-06-02 19:20:53 +0900

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: '/minifront-server/',
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
+  },
+  define: {
+    'process.env': {}
+  }
 });
